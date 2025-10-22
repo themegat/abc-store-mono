5d8dc79ac41a8b523192db224c2a6cbf2a00c015
themegat <tshepomotsoeneng0@gmail.com>
2025-10-22 18:34:26 +0200

    webapp deplyment improvements

--- abc-store-webapp/src/routes/Pages/Pages.tsx
+++ abc-store-webapp/src/routes/Pages/Pages.tsx
@@ -1,11 +1,14 @@
 import { useContext, useEffect, useState } from 'react';
 import { Routes } from 'react-router';
 
+import { Stack, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 
 import { AuthContext } from '@/components/auth/AuthContext';
 
 import routes from '..';
+import packageJson from '../../../package.json';
+const buildVersion = import.meta.env.VITE_BUILD_VERSION;
 import { getPageHeight, renderRoutes } from './utils';
 
 function Pages() {
@@ -23,6 +26,16 @@ function Pages() {
   return (
     <Box sx={{ height: (theme) => getPageHeight(theme) }}>
       <Routes location={defaultRoute}>{renderRoutes(routes)}</Routes>
+      <footer style={{ position: 'fixed', bottom: 0, backgroundColor: 'transparent', paddingLeft: 8 }}>
+        <Stack direction="row" gap={1}>
+          <Typography>
+            <small>Version: {packageJson.version}</small>
+          </Typography>
+          <Typography>
+            <small>Build: {buildVersion}</small>
+          </Typography>
+        </Stack>
+      </footer>
     </Box>
   );
 }
