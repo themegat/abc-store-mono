1dcfdce55ae7b39bf03b75e557714e8f178ed613
themegat <tshepomotsoeneng0@gmail.com>
2025-11-08 18:21:24 +0200

    Product details impl

--- abc-store-webapp/src/pages/ShoppingPage/ShoppingPage.tsx
+++ abc-store-webapp/src/pages/ShoppingPage/ShoppingPage.tsx
@@ -1,38 +1,53 @@
 import { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import { Grid2 as Grid, Stack, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Drawer,
+  Grid2 as Grid,
+  IconButton,
+  Stack,
+  Typography,
+  debounce,
+  useTheme,
+} from '@mui/material';
+
+import { t } from 'i18next';
 
 import Loading from '@/components/Loading';
 import ProductCard from '@/components/Product/ProductCard';
+import ProductDetails from '@/components/Product/ProductDetails';
 import { MaxPrice, ProductFilter, ProductFilterChanges } from '@/components/Product/ProductFilter';
 import { config } from '@/config';
 import { ProductDto, abcApi, useGetApiProductCategoriesQuery } from '@/store/api/abcApi';
-
-import backgroundShopImg from '../../assets/background/background_shop.webp';
-import { t } from 'i18next';
-import { useSelector } from 'react-redux';
 import { selectUser } from '@/store/slice/userSlice';
 
+import backgroundShopImg from '../../assets/background/background_shop.webp';
+
 const pageSize = 10;
-let pageNumber = 1;
 let products: ProductDto[] = [];
+let pageNumber = 1;
 let inStock = true;
 let categoryId = 0;
 let minPrice = 0;
 let maxPrice = MaxPrice;
+let loaded = false;
+const drawerWidth = 500;
+const debounceDelay = 500;
 
-function ShoppingPage() {
+const ShoppingPage = () => {
+  const theme = useTheme();
+  const [selectedProduct, setSelectedProduct] = useState<ProductDto | null>(null);
+
   const [fetchingProducts, setFetchingProducts] = useState(false);
-
   const { data: categoriesResponse } = useGetApiProductCategoriesQuery();
-
   const [getProducts] = abcApi.endpoints.getApiProductFilterByCurrencyCode.useLazyQuery();
 
   const user = useSelector(selectUser);
 
-  const fetchProducts = useCallback(() => {
+  const fetchProducts = useCallback(async () => {
     setFetchingProducts(true);
-    getProducts({
+    const response = await getProducts({
       currencyCode: user?.preferredCurrency || config.preferedCurrency,
       categoryId,
       inStock,
@@ -40,36 +55,25 @@ function ShoppingPage() {
       maxPrice,
       pageNumber: pageNumber,
       pageSize,
-    })
-      .unwrap()
-      .then((response) => {
-        if (response.items && response.items.length > 0) {
-          products = [...products, ...response.items];
-          pageNumber++;
-          setTimeout(() => {
-            observer.observe(document.querySelector('#load-more') as Element);
-          }, 1000);
-        }
-        setFetchingProducts(false);
-      });
-  }, [inStock, minPrice, maxPrice, categoryId]);
+    }).unwrap();
 
-  const onFilterChanged = (changes: ProductFilterChanges) => {
-    categoryId = changes.categoryId;
-    inStock = changes.inStock;
-    minPrice = changes.minPrice;
-    maxPrice = changes.maxPrice;
-    pageNumber = 1;
-    products = [];
-    fetchProducts();
-  };
+    if (response.items && response.items.length > 0) {
+      products = Array.from(new Set([...products, ...response.items]));
+      pageNumber++;
+    }
+    setFetchingProducts(false);
+    return response.items?.length;
+  }, [getProducts, user]);
 
   const observer = new IntersectionObserver(
     (entries) => {
-      entries.forEach((entry) => {
+      entries.forEach(async (entry) => {
         if (entry.isIntersecting) {
           observer.unobserve(entry.target);
-          fetchProducts();
+          const itemsLoaded = await fetchProducts();
+          if (itemsLoaded === pageSize) {
+            observer.observe(entry.target);
+          }
         }
       });
     },
@@ -79,12 +83,52 @@ function ShoppingPage() {
     },
   );
 
+  const test = useCallback(
+    debounce(
+      () => observer.observe(document.querySelector('#load-more') as Element),
+      debounceDelay * 2,
+    ),
+    [],
+  );
+  const setScrollObserver = (itemsLoaded: number) => {
+    if (itemsLoaded === pageSize) {
+      test();
+    }
+  };
+
+  const onFilterChanged = async (changes: ProductFilterChanges) => {
+    categoryId = changes.categoryId;
+    inStock = changes.inStock;
+    minPrice = changes.minPrice;
+    maxPrice = changes.maxPrice;
+    pageNumber = 1;
+    products = [];
+    const itemsLoaded = await fetchProducts();
+    setScrollObserver(itemsLoaded ?? 0);
+  };
+
   useEffect(() => {
-    fetchProducts();
+    const load = async () => {
+      const itemsLoaded = await fetchProducts();
+      setScrollObserver(itemsLoaded ?? 0);
+    };
+    if (!loaded) load();
+    loaded = true;
+
     return () => {
       observer.disconnect();
     };
   }, []);
+
+  const openProductDetails = (product: ProductDto) => {
+    observer.unobserve(document.querySelector('#load-more') as Element);
+    setSelectedProduct(product);
+  };
+
+  const closeProductDetails = () => {
+    setSelectedProduct(null);
+    setScrollObserver(pageSize);
+  };
 
   return (
     <>
@@ -98,39 +142,89 @@ function ShoppingPage() {
           minHeight: '100vh',
         }}
       >
-        <Grid container spacing={2}>
+        <Grid marginTop={10} container spacing={2}>
           <Grid size={12} textAlign="center">
             <Typography variant="h3">{t('routes.shop')}</Typography>
           </Grid>
           <Grid size={3}>
-            <Typography variant="h6" marginBottom={2}>
-              {t('productFilter.title')}
-            </Typography>
-            <ProductFilter
-              onFilterChange={onFilterChanged}
-              categories={categoriesResponse}
-            ></ProductFilter>
+            <Stack position="sticky" top={75}>
+              <Typography variant="h6" marginBottom={2}>
+                {t('productFilter.title')}
+              </Typography>
+              <ProductFilter
+                onFilterChange={onFilterChanged}
+                categories={categoriesResponse}
+              ></ProductFilter>
+            </Stack>
           </Grid>
           <Grid size={9}>
             <Stack gap={5} direction="row" flexWrap="wrap">
               {products.map((item, index) => (
                 <ProductCard
                   key={`product-${index}`}
+                  id={item?.id ?? 0}
                   sx={{ width: 240 }}
                   image={item?.thumbnailUrl ? item.thumbnailUrl : ''}
                   title={item?.name ?? ''}
                   price={item?.price ?? 0}
-                  currency={{}}
+                  stockQuantity={item?.stockQuantity ?? 0}
+                  currency={user?.preferredCurrency || config.preferedCurrency}
+                  onClick={() => openProductDetails(item)}
                 ></ProductCard>
               ))}
               {fetchingProducts && <Loading></Loading>}
             </Stack>
           </Grid>
-          {products.length >= 10 && <Stack width={'100%'} height={20} id="load-more"></Stack>}
+          {products.length >= 10 && <Stack width={'50%'} height={20} id="load-more"></Stack>}
         </Grid>
+        <Drawer
+          variant="temporary"
+          open={Boolean(selectedProduct)}
+          onClose={() => closeProductDetails()}
+          anchor="right"
+          sx={{
+            zIndex: 0,
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              backgroundColor:
+                theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.85)' : 'rgba(255, 255, 255, 0.85)',
+              padding: 3,
+              width: drawerWidth,
+              height: '92vh',
+              top: '8vh',
+              boxSizing: 'border-box',
+            },
+          }}
+        >
+          <Stack>
+            <Stack width="100%" alignItems="end">
+              <IconButton
+                onClick={() => closeProductDetails()}
+                sx={{ width: 40 }}
+                id="btn-close-product-details"
+              >
+                <CloseIcon />
+              </IconButton>
+            </Stack>
+            <>
+              {selectedProduct != null && (
+                <ProductDetails
+                  productId={selectedProduct.id ?? 0}
+                  image={selectedProduct.productImages ? selectedProduct.productImages[0] : ''}
+                  title={selectedProduct.name ?? ''}
+                  price={selectedProduct.price ?? 0}
+                  currency={user?.preferredCurrency || config.preferedCurrency}
+                  stockQuantity={selectedProduct.stockQuantity ?? 0}
+                  description={selectedProduct.description ?? ''}
+                />
+              )}
+            </>
+          </Stack>
+        </Drawer>
       </Stack>
     </>
   );
-}
+};
 
 export default ShoppingPage;
