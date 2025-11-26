2cbddcf37e133931b18c3719b281a243eb96c50a
themegat <tshepomotsoeneng0@gmail.com>
2025-11-26 17:42:17 +0200

    api deployment

--- abc-store-api/ABCStoreAPI/Database/Model/BaseEntity.cs
+++ abc-store-api/ABCStoreAPI/Database/Model/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace ABCStoreAPI.Database.Model;
 
 public class BaseEntity
 {
-    public int Id { get; set; }
+    public int? Id { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public string CreatedBy { get; set; } = string.Empty;
